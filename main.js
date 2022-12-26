84866e2e991e0d053d5e878d7067f562f13c5574
danielkldron <dc7027233@gmail.com>
2022-12-26 13:03:40 -0500

    agrege navegacion por mi paguina con los id

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const iconMenu = document.querySelector(".bx-menu")
 const menu = document.querySelector(".menu")
 
-let count = 0;
-
 iconMenu.addEventListener("click", function () {
     console.log(menu.classList.toggle("menu-show"));
 });
